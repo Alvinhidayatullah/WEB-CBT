c2ca8c62ec382fefb88cac8277487745fe680246
Alvin Hidayatullah <alvinhidayatullah3@gmail.com>
2026-07-02 06:54:09 +0700

    chore: trigger nextjs restart to reload prisma client cache

--- clean-exam/next.config.ts
+++ clean-exam/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Forced server restart to load new Prisma client
 };
 
 export default nextConfig;
